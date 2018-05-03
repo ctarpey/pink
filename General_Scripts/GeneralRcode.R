014649f8f5b39cafb2ceed431d9e4832e92534a8
ctarpey <ctarpey@uw.edu>
2018-05-03 11:07:23 -0700

    LD analysis for marker panel

--- General_Scripts/GeneralRcode.R
+++ General_Scripts/GeneralRcode.R
@@ -16,6 +16,7 @@ library(hierfstat)
 library(adegenet)
 library(argparse)
 library(stringi)
+library(reshape2)
 
 
 ####<------------------------------------------------START HERE,
@@ -42,6 +43,30 @@ genotype_file_t<-gsub("X","",colnames(genotype_file))
 allGenos_oneSNP_temp_tags<-data.frame(str_split_fixed(allGenos_oneSNP_temp$Locus,"_",2))
 
 FST_file_ASIAeven_sort <- FST_ASIA_even[order(FST_ASIA_even$ASIA_even, decreasing=TRUE),]
+
+#change the variable to the number you want 
+NAeven_var <- 500 #<--------------------------------------------dictates the number of loci
+NAeven_set <- FST_file_NAeven_sort_inRange[1:NAeven_var,]
+NAeven_set_avgFST <- mean(NAeven_set$NA_even)
+NAeven_set_maxFST <- max(NAeven_set$NA_even)
+NAeven_set_minFST <- min(NAeven_set$NA_even)
+cat("NAeven FST max, min, average: ",NAeven_set_maxFST, NAeven_set_minFST, NAeven_set_avgFST )
+
+## Add these list of failed to the list of passed to get a list to pull the sequences to make a FASTA file 
+Even_failed_passed_list <- append(NA_ASIA_even_union_FST_sort$Locus, Even_failed_union)
+
+# merge the SNP_inRange designations
+Even_FST_file <- merge(Even_FST_file, Even_Tag_Flags, by = "Tag")
+
+#create new data frame from columns of another
+FST_NA_even<- Even_FST_file[,c("Locus","NA_even","Even_SNPs_InRange")]
+
+#add new empty column to the end of each data frame for the flags
+Even_SNPS$Btw_17_73 <- NA
+
+#delete a column
+#add new empty column to the end of each data frame for the flags
+Even_SNPS$Btw_17_73 <- NULL
 
 ### Set opperations
 
@@ -85,6 +110,10 @@ locusGenoRate<-apply(allGenos,2,function(x) 1-(sum(x=="0000")/dim(allGenos)[1]))
 
 
 
+
+
+#<----------------------------------------PLOTTING
+
 ###@@@@@@@@@@@@@@@@@@@@@@Plots for the Manuscript, have labels as Principal components
 ALL_col<- c("#cd5490","#cd5490","#c9cf4a","#c9cf4a","#7297ee","#7297ee","#61005e","#61005e", "#677f3e","#677f3e","#00158a","#00158a","#e0957e","#e0957e")
 
@@ -101,3 +130,35 @@ ggplot(data = ALL_geo) + geom_point(aes(x = V3, y = V4,  color = POPNAME, shape 
   labs(list(title= "All Populations Principal Components 1 & 2", x = "Principal Component 1 (30.24%)", y = "Principal Component 2 (13.83%)", size = 30))
 
 dev.off()
+
+
+
+#histograms with bars next to eachother 
+
+Histo_7_colors<- c("#6e016b","#88419d","#8c6bb1","#8c96c6","#7bccc4",'#4eb3d3','#2b8cbe')
+Histo_14_colors<- c("#6e016b","#6e016b","#88419d","#88419d","#8c6bb1","#8c6bb1","#8c96c6","#8c96c6","#7bccc4","#7bccc4",'#4eb3d3','#4eb3d3','#2b8cbe','#2b8cbe')
+E_O_Colors <- c("#00158a", "#c9cf4a")
+
+even_snps_counts <- as.data.frame(table(Even_SNPS$Tag))
+even_snps_counts <- as.data.frame(table(even_snps_counts$Freq))
+colnames(even_snps_counts) <-c("SNPs","Even_Count")
+even_snps_counts
+
+odd_snps_counts <- as.data.frame(table(Odd_SNPS$Tag))
+odd_snps_counts <- as.data.frame(table(odd_snps_counts$Freq))
+colnames(odd_snps_counts) <-c("SNPs","Odd_Count")
+odd_snps_counts
+
+snp_counts<-merge(odd_snps_counts,even_snps_counts, by = "SNPs")
+snp_counts_melt <- melt(snp_counts)
+
+#pdf("Z:/WORK/TARPEY/Exp_Pink_Pops/Analysis/MarkerSelection/PLOTs_Snps_per_Haplotype_lineage.pdf", width = 9, height = 7)
+
+# histogram with the even and odd next to eachother in one plot 
+ggplot(data=snp_counts_melt, aes(snp_counts_melt$SNPs, snp_counts_melt$value)) + theme_bw() +
+  geom_bar(aes(fill= snp_counts_melt$variable), alpha = 0.5, position = "dodge", stat="identity") +
+  xlab("Number of SNPs per Haplotype") + ylab("Count") + ggtitle("Variable SNPs per Haplotype ") +
+  scale_fill_manual(values = E_O_Colors, labels = c("Odd", "Even")) + labs(fill="Lineage")
+
+#dev.off()  
+
